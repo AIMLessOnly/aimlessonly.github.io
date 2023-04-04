535edb72a3f41fda8bf4e0e16e116112b961d2f3
yashvarma2004 <yv44905@gmail.com>
2023-04-05 00:12:43 +0530

    updated-12:00

---
+++ images/bgt_files/register2.php
@@ -0,0 +1,17 @@
+var com_adswizz_register_PROTOCOL_VERSION = '2.2.0';
+
+if (typeof com_adswizz_synchro_listenerid === 'undefined') {
+    var com_adswizz_synchro_listenerid = '65f20ea2ed9d9bc0e46f8184c9f8fd1a';
+}
+else {
+    com_adswizz_synchro_listenerid = '65f20ea2ed9d9bc0e46f8184c9f8fd1a';
+}
+
+if (typeof com_adswizz_synchro_listnerid === 'undefined') {
+    var com_adswizz_synchro_listnerid = com_adswizz_synchro_listenerid; // backwards compatibility
+}
+else {
+    com_adswizz_synchro_listnerid = com_adswizz_synchro_listenerid; // backwards compatibility
+}
+var aw_0_req_gdpr = false;
+var us_privacy = '';
